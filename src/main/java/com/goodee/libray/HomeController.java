30f59df17e7cd3f24ccdfe1047e73ecfb26d8535
gilsuyeon <tkfkdgo7557@gmail.com>
2024-01-12 12:33:10 +0900

    [길수연] 홈페이지 생성

--- src/main/java/com/goodee/libray/HomeController.java
+++ src/main/java/com/goodee/libray/HomeController.java
@@ -1,39 +1,25 @@
 package com.goodee.libray;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+// 겟으로 진입햇을때 어떤 작업을 실행하는가 
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger LOGGER =
+			LoggerFactory.getILoggerFactory(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	// 겟으로 진입햇을때 어떤 작업을 실행하는가를 컨트롤에 적어줌
+	// / 면서 GET 방식일때 이 URL를 타겠다
+	@RequestMapping(value= {"","/"}, method=RequestMethod.GET)
+	public String home() {
+		LOGGER.info("[HomeController] home();");
 		return "home";
 	}
+	}
 	
-}
+
